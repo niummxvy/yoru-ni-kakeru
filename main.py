9cedd075c7b44c200a510c97d32b62e6399e2bbf
李順斌 <52768403+niummxvy@users.noreply.github.com>
2021-03-15 21:27:51 +0800

    MakeCode 專案的初始檔案

---
+++ main.py
@@ -0,0 +1,6 @@
+basic.show_string("qwertyuiopasdfghjklzxcvbnm")
+music.play_melody("F - G D - A E G ", 120)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,12 @@
+music.setVolume(64)
+music.playMelody("E G A - F E D C ", 235)
+music.playMelody("D A G A E D C C ", 235)
+music.playMelody("C C C C C C - - ", 235)
+basic.pause(750)
+music.playMelody("E G A - F E D B ", 235)
+music.playMelody("A G G A B C5 - E ", 235)
+music.playMelody("D C C C C C C C ", 235)
+basic.pause(2000)
+basic.forever(function () {
+	
+})
